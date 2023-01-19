55457c072494433eaec452a47c7d80a46afb6ca7
jaibalaji2509 <balaravanan81726@gmail.com>
2023-01-19 15:15:21 +0530

    First release of Hello World!

---
+++ src/Constants.js
@@ -0,0 +1,102 @@
+import AUTOMATION from "./Assets/Projects/3automation.jpg";
+import COAST from "./Assets/Projects/coast.jpg";
+import DROPSHIP from "./Assets/Projects/dropship.jpg";
+import GOOD_FOOD_DISCOUNTS from "./Assets/Projects/goodFoodDiscounts.png";
+import GUAMEDIA from "./Assets/Projects/guamedia.jpg";
+import HSP from "./Assets/Projects/hsp.png";
+import COSMOS from "./Assets/Projects/kposty.png";
+import TECHSPEC from "./Assets/Projects/techspec.png";
+import TRAVELWELL from "./Assets/Projects/travelwell.png";
+
+export const PROJECTS = [
+  {
+    image: COSMOS,
+    name: "KSmacc",
+    technologyUsed: "Html, Css, BootStrap",
+    description:
+      "KSmacc CLOSED communication for all types of institutions like Clubs, Associations and Societies whether they are multinational or big or small.",
+    url: "https://ksmacc.in/",
+  },
+  // {
+  //   image: TRAVELWELL,
+  //   name: "travlwell.com",
+  //   technologyUsed: "Shopify, React, typescript, polaris, GraphQL",
+  //   description:
+  //     "Custom Luggage Tags and Passport Holders engraved with everywhere you've traveled and everywhere you will travel in the future.",
+  //   url: "https://travlwell.com/",
+  // },
+  // {
+  //   image: COAST,
+  //   name: "Coast",
+  //   technologyUsed: "ReactNative, Javascript, Redux",
+  //   description:
+  //     "Team collaboration mobile app and online services like slack Message people and groups without sharing phone numbers, communicate with managers and staff all in one place. and discuss or assign tasks to get stuff done",
+  //   url: "https://play.google.com/store/apps/details?id=com.fomo.android.app",
+  // },
+  // {
+  //   image: AUTOMATION,
+  //   name: "3automation.com (RPA)",
+  //   technologyUsed: "ReactJs, Typescript, Redux",
+  //   description:
+  //     "The next-gen Robotic Process Automation (#RPA) platform for retail and telecom domains. The ultimate automation platform for your Shopify, Email, E- commerce shop",
+  //   url: "https://www.3automation.com/",
+  // },
+  // {
+  //   image: GUAMEDIA,
+  //   name: "GuaMedia",
+  //   technologyUsed: "ReactNative, Javascript, Redux",
+  //   description:
+  //     "Social media mobile app similar to Twitter for china. Social networking GUOMEDIA is an all-in-one social networking platform where users can build their own user profiles, create their own posts, Livestream broadcast and follow each other",
+  //   url: "https://play.google.com/store/apps/details?id=com.tiu.guo.media",
+  // },
+  // {
+  //   image: TECHSPEC,
+  //   name: "TechSpec",
+  //   technologyUsed: "ReactJs, Typescript, Redux",
+  //   description:
+  //     "TechSpec is e-commerce web app. User can compare his product and purchange. This had a dashboard web app which had the data of orders placed and type of user.",
+  //   url: "",
+  // },
+  // {
+  //   image: HSP,
+  //   name: "HSP (Health Solution Plus)",
+  //   technologyUsed: "ReactJs, Javascript, Redux",
+  //   description:
+  //     "This is a Healthcare sector project which would be developed as replica of a desktop application. My responsibilities in this project are to develop UI, perform searching of medical codes and categories, addition/updation of codes and categories.",
+  //   url: "",
+  // },
+  // {
+  //   image: DROPSHIP,
+  //   name: "DropShip Shopify App",
+  //   technologyUsed:
+  //     "Shopify, React, typescript, polaris, GraphQL, Nodejs, Koajs",
+  //   description:
+  //     "It is customized clothes shopify app which fetch order from store and give option to customize product and send detail to store owner. Also maintain tracking etc of product.",
+  //   url: "",
+  // },
+  // {
+  //   image: GOOD_FOOD_DISCOUNTS,
+  //   name: "GoodFoodDiscounts",
+  //   technologyUsed: "ReactNative, Javascript, Redux",
+  //   description:
+  //     "Good food discouts app is similar like swiggy. Food and hotel can be searched and can be order. Even has facility to add coupan. You can add food in favourite list.",
+  //   url: "",
+  // },
+];
+
+export const SKILLS = [
+  { name: "Html", initialRating: 5 },
+  { name: "CSS", initialRating: 4 },
+  { name: "Bootstrap", initialRating: 4 },
+  { name: "JavaScript", initialRating: 3 },
+  { name: "ReactJs", initialRating: 4 },
+  { name: "Git", initialRating: 4 },
+];
+
+export const TOOLS = [
+  "Visual Studio Code",
+  "Git",
+  "Chrome DevTools",
+  "Npm (Node Package Manager)",
+  "Postman",
+];
---
+++ src/components/Home/TypeWriter.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function TypeWriter() {
+  return (
+    <Typewriter
+      options={{
+        strings: ["Front End Developer", "Software Engineer"],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default TypeWriter;
